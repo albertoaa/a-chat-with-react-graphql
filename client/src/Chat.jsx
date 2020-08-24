9352bb055ce8837e91e88a3c1a4075babf5ef697
Alberto Aragon <ronin8503@gmail.com>
2020-08-24 11:29:55 -0500

    Layout of messages and input for new messages

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,14 +1,115 @@
 import React from 'react';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client';
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
-  uri: 'https://localhost:4000',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: user == messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: '0.5em',
+                border: '2px solid #e5e6ea',
+                borderRadius: 25,
+                textAlign: 'center',
+                fontSize: '18pt',
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user == messageUser ? '#58bf56' : '#e5e6ea',
+              color: user == messageUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
 const Chat = () => {
-  return <div>I'm a chat window</div>;
+  const [state, setState] = React.useState({
+    user: 'Alberto',
+    content: '',
+  });
+
+  return (
+    <Container>
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <FormInput
+            label='User'
+            value={state.user}
+            onChange={(event) =>
+              setState({
+                ...state,
+                user: event.target.value,
+              })
+            }
+          />
+        </Col>
+        <Col xs={8}>
+          <FormInput
+            label='Content'
+            value={state.content}
+            onChange={(event) =>
+              setState({
+                ...state,
+                content: event.target.value,
+              })
+            }
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default () => (
